3168239fb83a0f13f832b96de5826daa4b5a5489
Gannon Talken <gannon.talken28@gmail.com>
2024-01-22 21:12:09 -0600

    Candidate Testing

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,9 +5,9 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName= ""
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question= "Who was the first AMerican woman in space?"
-let correctAnswer; "Sally Ride"
-let candidateAnswer; " "
+let question= "Who was the first American woman in space?"
+let correctAnswer= "Sally Ride"
+let candidateAnswer= ""
 
 
 //TODO: Variables for Part 2
@@ -18,27 +18,37 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
+  candidateName=input.question("Enter your name: ")
 }
-prompt("Please enter yout name")
+
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-console.log(questions)
-console.log("Answer:")
-candidateAnswers=askQuestion
+  let questions =['Who was the first American woman in space?','True or false: 5 kilometer==5000 meters','(5+3)/2*10=?',"Given the array[8,'Orbit','Trajectory',45],what entry is at index 2?",'What is the minimum crew size for the ISS?']
+  let correctAnswers= ["Sally Ride","true","40","Trajectory","3"]
+  let userResponse=[]
+  for (let i=0; i <questions.length;i++){
+    let userReponse= prompt(questions[i]);
+    userResponse.push(userResponse);
+  }
+ 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+for (let i=0;i<questions.length;i++){
+  console.log(`Question ${i + 1}: ${questions[i]}`);
+  console.log(`User's Response: ${userResponses[i]}`);
+  console.log(`Correct Answer: ${correctAnswers[i]}`);
+}
 
-
-
+}
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
 
   return grade;
-}
+
 
 function runProgram() {
   askForName();
